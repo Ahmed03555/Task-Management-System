2ff89564912d874c9eacbe92a2910fba2f7a1747
Ahmed03555 <ahmedyass160@gmail.com>
2026-09-11 14:59:15 +0300

    Validation

---
+++ TaskManagement.Application/Common/Exceptions/ValidationException.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagement.Application.Common.Exceptions
+{
+    #region ValidationException
+    public class ValidationException : Exception
+    {
+        public IDictionary<string, string[]> Errors { get; }
+
+        public ValidationException(IEnumerable<ValidationFailure> failures) : base("One or more validation failures occurred.")
+        {
+            Errors = failures
+                .GroupBy(x => x.PropertyName)
+                .ToDictionary(x => x.Key, x => x.Select(f => f.ErrorMessage).ToArray());
+        }
+    } 
+    #endregion
+}

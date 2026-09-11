862636e8db98a2967f282c2d4ef120e60c6ee6ac
Ahmed03555 <ahmedyass160@gmail.com>
2026-09-11 17:34:40 +0300

    GetTasksByProject

---
+++ TaskManagement.Application/Model/Tasks/Commands/Queries/GetTasksByProject/GetTasksByProjectQuery.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagement.Application.Model.Tasks.Commands.Queries.GetTasksByProject
+{
+    #region GetTasksByProjectQuery
+    public record GetTasksByProjectQuery(Guid ProjectId) : IRequest<Result<List<TaskDto>>>; 
+	#endregion
+}
---
+++ TaskManagement.Application/Model/Tasks/Commands/Queries/GetTasksByProject/GetTasksByProjectQueryHandler.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagement.Application.Common.Interface;
+
+namespace TaskManagement.Application.Model.Tasks.Commands.Queries.GetTasksByProject
+{
+    #region GetTasksByProjectQueryHandler
+    public class GetTasksByProjectQueryHandler : IRequestHandler<GetTasksByProjectQuery, Result<List<TaskDto>>>
+    {
+        private readonly IApplicationDbContext _ApplicationDbContext;
+        private readonly IMapper _Mapper;
+
+        public GetTasksByProjectQueryHandler(IApplicationDbContext applicationDbContext, IMapper mapper)
+        {
+            _ApplicationDbContext=applicationDbContext;
+            _Mapper=mapper;
+        }
+
+        public async Task<Result<List<TaskDto>>> Handle(GetTasksByProjectQuery request, CancellationToken cancellationToken)
+        {
+            var tasks = await _ApplicationDbContext.tasks.Where(t => t.ProjectId == request.ProjectId).ProjectTo<TaskDto>(_Mapper.ConfigurationProvider).ToListAsync(cancellationToken);
+
+            return Result<List<TaskDto>>.Success(tasks);
+        }
+    } 
+    #endregion
+}

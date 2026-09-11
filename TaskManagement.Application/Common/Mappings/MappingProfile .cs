59545f8f40d744db805b66a10e6655f03d33d804
Ahmed03555 <ahmedyass160@gmail.com>
2026-09-11 16:31:40 +0300

    Mapping && TaskDto

---
+++ TaskManagement.Application/Common/Mappings/MappingProfile .cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagement.Application.Model.Tasks.Commands.Queries.GetTasksByProject;
+using TaskManagement.Domain.Entities;
+
+namespace TaskManagement.Application.Common.Mappings
+{
+    #region Mapping
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<TaskItem, TaskDto>()
+                .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Satus.ToString()))
+                .ForMember(dest => dest.Priority, opt => opt.MapFrom(src => src.Priority.ToString()));
+        }
+    } 
+    #endregion
+}
---
+++ TaskManagement.Application/Model/Tasks/Commands/Queries/GetTasksByProject/TaskDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagement.Application.Model.Tasks.Commands.Queries.GetTasksByProject
+{
+    #region TaskDto
+    public class TaskDto
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; } = default!;
+        public string? Description { get; set; }
+        public string Status { get; set; } = default!;
+        public string Priority { get; set; } = default!;
+        public DateTime? DueDate { get; set; }
+    } 
+    #endregion
+}

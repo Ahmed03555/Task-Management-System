30f25934c4b2590b255b9eb3832be44227506cb4
Ahmed03555 <ahmedyass160@gmail.com>
2026-09-12 11:41:43 +0300

    PasswordHasher

---
+++ TaskManagement.Application/Common/Interface/IPasswordHasher.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagement.Application.Common.Interface
+{
+    #region PasswordHasher
+    public interface IPasswordHasher
+    {
+        string Hash(string password);
+        bool Verify(string password, string hashedPassword);
+    } 
+    #endregion
+}
---
+++ TaskManagement.Infrastructure/Services/PasswordHasher.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagement.Application.Common.Interface;
+
+namespace TaskManagement.Infrastructure.Services
+{
+    public class PasswordHasher : IPasswordHasher
+    {
+        #region PasswordHasher
+        public string Hash(string password)
+        {
+            // Implementation for hashing password
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+
+        public bool Verify(string password, string hashedPassword)
+        {
+            // Implementation for verifying password
+            return BCrypt.Net.BCrypt.Verify(password, hashedPassword);
+        } 
+        #endregion
+    }
+}
